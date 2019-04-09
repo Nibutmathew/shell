ef1317ec5765e90aebe58c6cebfde69475796546
athira-das <athiradas9822@gmail.com>
2019-04-09 20:32:33 +0530

    14th program

---
+++ armstronglimit.sh
@@ -0,0 +1,17 @@
+echo "enter the two numbers"
+read a b
+for (( i=$a ; i<=$b ; i++ ))
+do
+n=$i
+s=0
+while [ $n -gt 0 ]
+do
+r=`expr $n % 10`
+n=`expr $n / 10`
+s=`expr $s + $r \* $r \* $r`
+done 
+if [ $s -eq $i ]
+then
+echo $i
+fi
+done
